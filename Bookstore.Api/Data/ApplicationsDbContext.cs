e6ee73b65fce04a40002c31f934cf318d716f9de
1LeandroLV <leandro.verdugo02@gmail.com>
2026-06-04 00:59:44 +0200

    Create ApplicationDbContext and configure DbSets

---
+++ Bookstore.Api/Data/ApplicationsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookStore.Api.Models;
+
+namespace BookStore.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
