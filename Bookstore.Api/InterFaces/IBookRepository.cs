d00f16c233de7eba504448109fc9a5ee6e9b757a
1LeandroLV <leandro.verdugo02@gmail.com>
2026-06-04 14:40:20 +0200

    Implement generic repository and book repository pattern

---
+++ Bookstore.Api/InterFaces/IBookRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Api.Models;
+
+namespace BookStore.Api.Interfaces
+{
+    public interface IBookRepository : IGenericRepository<Book>
+    {
+        Task<IEnumerable<Book>> GetBooksWithAuthorsAsync();
+    }
+}
---
+++ Bookstore.Api/Repositories/BookRepository.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Api.Data;
+using BookStore.Api.Interfaces;
+using BookStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Api.Repositories
+{
+    public class BookRepository : GenericRepository<Book>, IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksWithAuthorsAsync()
+        {
+            return await _context.Books
+                .Include(b => b.Author)
+                .ToListAsync();
+        }
+    }
+}
