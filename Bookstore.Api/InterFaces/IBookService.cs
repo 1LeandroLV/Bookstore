5d1fad4416043f05d322e95b0831576ed2ed0dc7
1LeandroLV <leandro.verdugo02@gmail.com>
2026-06-04 15:12:37 +0200

    Add book service layer with repository dependency

---
+++ Bookstore.Api/InterFaces/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Api.Models;
+
+namespace BookStore.Api.Interfaces
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+
+        Task<Book?> GetBookByIdAsync(int id);
+
+        Task CreateBookAsync(Book book);
+
+        Task UpdateBookAsync(Book book);
+
+        Task DeleteBookAsync(int id);
+    }
+}
---
+++ Bookstore.Api/Services/BookService.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.Api.Interfaces;
+using BookStore.Api.Models;
+
+namespace BookStore.Api.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllBooksAsync()
+        {
+            return await _bookRepository.GetBooksWithAuthorsAsync();
+        }
+
+        public async Task<Book?> GetBookByIdAsync(int id)
+        {
+            return await _bookRepository.GetByIdAsync(id);
+        }
+
+        public async Task CreateBookAsync(Book book)
+        {
+            await _bookRepository.AddAsync(book);
+        }
+
+        public async Task UpdateBookAsync(Book book)
+        {
+            await _bookRepository.UpdateAsync(book);
+        }
+
+        public async Task DeleteBookAsync(int id)
+        {
+            await _bookRepository.DeleteAsync(id);
+        }
+    }
+}
