c0477219cc4b67f2e356c43850a33f03666a2eda
1LeandroLV <leandro.verdugo02@gmail.com>
2026-06-05 14:08:25 +0200

    Complete CRUD operations for books and authors

--- Bookstore.Api/Dtos/UpdateBookDto.cs
+++ Bookstore.Api/Dtos/UpdateBookDto.cs
@@ -1,11 +1,16 @@
-﻿namespace BookStore.Api.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Api.DTOs
 {
     public class UpdateBookDto
     {
+        [Required]
         public string Title { get; set; } = string.Empty;
 
+        [Range(1, double.MaxValue)]
         public double Price { get; set; }
 
+        [Range(1, int.MaxValue)]
         public int AuthorId { get; set; }
     }
 }
